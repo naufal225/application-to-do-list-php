19c143f70c70fea541c75a91eb0799f7cd51b66c
naufal225 <naufalmarufashrori225@gmail.com>
2024-07-24 08:40:06 +0700

    Memperbaiki bug

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if(isset($_SESSION['username'])) {
     $row = mysqli_fetch_assoc($result);
     $username = $row['username'];
     $id = $row['id_user'];
-} else {
+} elseif(isset($_COOKIE['jml'])) {
     $id = $_COOKIE['jml'];
     $result = mysqli_query($conn, "SELECT * FROM tbl_user WHERE id_user = $id");
     $row = mysqli_fetch_assoc($result);
@@ -23,7 +23,6 @@ if(isset($_SESSION['username'])) {
 }
 
 $s = isset($_GET['s']) ? $_GET['s'] : '';
-
 $lists = getAllListWithIdUserAndContent($id, $s);
 
 $no = 1;
